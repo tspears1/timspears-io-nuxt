9c8746679aa9fd764ad3af5646317f70e4f6b2f2
Tim Spears <timspears@imarc.com>
2023-02-15 19:42:01 -0500

    init commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="nav in data.navigation" :key="nav.slug">
+        <a :href="`/${nav.slug}`">{{ nav.title }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { data } = useSanityQuery(groq`
+  *[_type == "settings"]{
+    siteEyebrow,
+    siteTitle,
+    navigation[] -> {
+      title,
+      "slug": slug.current
+    }
+  }[0]
+`)
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/sanity'],
+    sanity: {
+        projectId: 'z0o6cgrz',
+    },
+    css: ['@/assets/scss/styles.scss']
+})
+
+// swiper
+// pinia
+// image
+// vueuse
+// headless ui
+// formkit
+// nuxt-viewport
+// radash
