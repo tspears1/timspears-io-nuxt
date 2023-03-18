0b3f863277fc612af6b991d3ed2186aae3fd37c1
Tim Spears <timspears@imarc.com>
2023-03-17 22:34:23 -0400

    fitler cards start

---
+++ components/Filter/Card.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+const props = defineProps({
+    card: Object,
+})
+
+const imageRef = ref()
+
+</script>
+
+<template>
+    <article class="filter-card">
+        <div class="filter-card__content">
+            <div class="filter-card__wrapper">
+                <div class="filter-card__mask">
+                    <div class="filter-card__header">
+                        <div class="filter-card__eyebrow">
+                            {{ card.eyebrow }}
+                        </div>
+                        <div class="filter-card__services">
+                            <IconService
+                            v-for="service in card.services"
+                            :key="`${service}-${card.title}`"
+                            :icon="service"
+                            class="filter-card__icon"
+                            />
+                        </div>
+                    </div>
+
+                    <h3 class="filter-card__title">
+                        <a
+                            :href="card.url"
+                            class="filter-card__link"
+                        >
+                            {{ card.title }}
+                        </a>
+                    </h3>
+
+                    <div class="filter-card__cta">
+                        <span class="filter-card__cta-text">View Project</span>
+                        <div class="filter-card__cta-icon"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="filter-card__media">
+            <img
+                class="filter-card__img lazyload"
+                ref="imageRef"
+                :src="card.image"
+            />
+        </div>
+    </article>
+</template>
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,14 +1,3 @@
-<template>
-    <main class="page-wrapper">
-        <Hero v-if="data" :title="data.pageTitle ?? null" :eyebrow="data.eyebrow"/>
-        <FilterBar />
-        <section class="section section--dark-matrix">
-
-        </section>
-        <FilterModal />
-    </main>
-</template>
-
 <script setup>
 const { data } = useSanityQuery(groq`
     *[_type == "page" && slug.current == 'work']{
@@ -17,4 +6,52 @@ const { data } = useSanityQuery(groq`
     }[0]
 `)
 
+const cards = [
+    {
+        eyebrow: 'Website Redesign',
+        title: 'myOrthos',
+        url: 'https://www.imdb.com',
+        services: ['frontend', 'motion'],
+        image: 'https://source.unsplash.com/random/800x800/?scifi/'
+    },
+    {
+        eyebrow: 'Icon Set',
+        title: 'Cookie Jar Kitchen',
+        url: 'https://www.imdb.com',
+        services: ['graphic', 'backend', 'frontend', 'web', 'motion', 'brand'],
+        image: 'https://source.unsplash.com/random/800x800/?woods/'
+    },
+    {
+        eyebrow: 'Website Redesign',
+        title: 'Ember Gardens',
+        url: 'https://www.imdb.com',
+        services: ['backend', 'frontend', 'web', 'motion'],
+        image: 'https://source.unsplash.com/random/800x800/?space/'
+    },
+    {
+        eyebrow: 'Brand New Logo',
+        title: 'Marge Bar',
+        url: 'https://www.imdb.com',
+        services: ['brand'],
+        image: 'https://source.unsplash.com/random/800x800/?portland,girl/'
+    },
+]
+
 </script>
+
+<template>
+    <main class="page-wrapper">
+        <Hero v-if="data" :title="data.pageTitle ?? null" :eyebrow="data.eyebrow"/>
+        <FilterBar />
+        <section class="section section--dark-matrix">
+            <div class="filter-grid">
+                <FilterCard
+                    v-for="card in cards"
+                    :key="card.title"
+                    :card="card"
+                />
+            </div>
+        </section>
+        <FilterModal />
+    </main>
+</template>
