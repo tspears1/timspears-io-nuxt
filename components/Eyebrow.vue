aeb8e37da2d2557d4a7a07b6e0a69a452d10ecb5
Tim Spears <timspears@imarc.com>
2023-07-10 23:08:32 -0400

    finish text-heading-block

--- components/Eyebrow.vue
+++ components/Eyebrow.vue
@@ -37,6 +37,7 @@ const props = defineProps({
 
 const eyebrowRef = ref()
 const stem = ref()
+const iconRef = ref()
 const splitText = ref(null)
 
 const parentClass = computed(() => props.block ? `${props.block}-eyebrow` : 'eyebrow')
@@ -48,7 +49,9 @@ const generateClass = (phrase) => `${parentClass.value}__${phrase}`
 const enterSequence = computed(() => {
     let sequence = []
     if ( !props.icon ) {
-        sequence = [[stem.value, { scaleX: [ 0.001, 1] }, { duation: 0.5, easing: cubicBezier.easeOutQuint, at: props.offset }]]
+        sequence = [[ stem.value, { scaleX: [ 0.001, 1] }, { duation: 0.5, easing: cubicBezier.easeOutQuint, at: props.offset }]]
+    } else {
+        sequence = [[ iconRef.value, { y: ['calc(100% + 0.25em)', 0] }, { duration: 0.25, at: props.offset } ]]
     }
     sequence = [...sequence, [letters.value, { y: ['calc(100% + 0.25em)', 0] }, { delay: stagger(0.075), duration: 0.125, at: props.icon ? props.offset : '-0.5' } ]]
     return sequence
@@ -59,6 +62,8 @@ const exitSequence = computed(() => {
     sequence = [...sequence, [letters.value, { y: [0, 'calc(100% + 0.25em)'] }, { delay: stagger(0.075, { from: 'last' }), duration: 0.125, at: props.offset } ]]
     if ( !props.icon ) {
         sequence = [...sequence, [stem.value, { scaleX: [ 1, 0.001] }, { duation: 0.25, easing: cubicBezier.easeOutQuint, at: props.icon ? props.offset : '-0.5' }]]
+    } else {
+        sequence = [...sequence, [iconRef.value, { y: [0, 'calc(100% + 0.25em)'] }, { duration: 0.25, at: props.offset } ]]
     }
     return sequence
 })
@@ -84,7 +89,12 @@ defineExpose({
 
 <template>
     <div :class="[parentClass, { '-is-splitting': !frozen }]" ref="eyebrowRef">
-        <div v-if="icon" :class="generateClass('icon')" />
+        <div v-if="icon" :class="generateClass('icon')">
+            <div :class="generateClass('icon-wrapper')" ref="iconRef">
+                <IconService :icon="icon" />
+            </div>
+        </div>
+
         <div v-else :class="generateClass('stem')" ref="stem" />
 
         <div v-if="frozen" :class="generateClass('text')">
