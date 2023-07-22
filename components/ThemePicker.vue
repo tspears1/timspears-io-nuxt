e41f711de988670375a9fd3ccb27ed9176d790a8
Tim Spears <timspears@imarc.com>
2023-07-22 19:30:24 -0400

    start footer

---
+++ components/ThemePicker.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="theme-picker">
+        Current Theme: {{ theme }}
+        <div class="theme-picker__icon">
+            <!-- eyedrop icon -->
+
+        </div>
+    </div>
+</template>
--- utils/numbers.js
+++ utils/numbers.js
@@ -4,12 +4,12 @@ const pad = (n, before = '0') => (before + n).slice((before.length + 1) * -1)
  *
  * @function remToPx
  * @description Convert rem string to pixel number
- * @param {string} rem
+ * @param {[string, number]} rem
  * @param {number} rootSize
  * @returns {number}
  *
  */
-const remToPx = (rem, rootSize = 16) => rem.indexOf('rem') !== -1 ? parseFloat(rem) * rootSize : parseFloat(rem)
+const remToPx = (rem, rootSize = 16) => rem.toString().includes('rem') ? parseFloat(rem) * rootSize : parseFloat(rem)
 
 
 export { pad, remToPx }
